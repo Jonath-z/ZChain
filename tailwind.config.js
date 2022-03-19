c7ae595e3892123a674c4a3f05ccc26bd315e52a
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-19 18:21:21 +0200

    new font added in the config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Orbitron: ['Orbitron', 'sans-serif'],
+        Roboto: ['Roboto', 'sans-serif'],
+        Mulish: ['Mulish', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
