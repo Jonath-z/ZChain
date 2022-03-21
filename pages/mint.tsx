fa9df7bbbceb1ae38fc510fe950b24e43867481a
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-21 20:40:53 +0200

    mint page

---
+++ pages/mint.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+import React from 'react';
+import CreateNft from '../components/CreateNft';
+
+const Mint: NextPage = () => {
+    return (
+            <CreateNft />
+    );
+}
+
+export default Mint;
