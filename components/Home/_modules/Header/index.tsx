31ceb0050d2bd31b37bb1323c3dc4cf38679cb8d
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-21 20:38:48 +0200

    marketItem page

---
+++ components/Home/_modules/Header/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+('next/image');
+import full_logo from '../../../../assets/images/zchain-logo.png';
+import Search from './Search';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <div className="flex flex-row justify-between items-center px-10 fixed top-0 right-52 left-0">
+      <Image src={full_logo} alt="ZChain" width="130" height="130" />
+      <Search />
+      <ul className="text-white flex flex-row justify-between w-96 font-Roboto">
+        <li><Link href="/market">Marketplace</Link></li>
+        <li>Artists</li>
+      </ul>
+      <button className="bg-[#ffa503] py-3 px-10 rounded-md hover:bg-opacity-80">
+        Connect Wallet
+      </button>
+    </div>
+  );
+};
+
+export default Header;
