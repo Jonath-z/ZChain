a6360b6c9fad3c9436bb43efe0f2c95ce66b1470
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-26 23:33:02 +0200

    nft address reviewed

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const nftAddress = " 0xc208a760F2d8809cc779BCF3bE39EDd4358B3c7f";
+export const nftAddress = "0xc208a760F2d8809cc779BCF3bE39EDd4358B3c7f";
 export const nftMarketplaceAddress = "0x3658Ef8cB0504A21ad1e0CeD2936F05B7E4dBA8a";
 
 ///////////////////////////LOCAL NODE /////////////////////////
